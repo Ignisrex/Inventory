93e81e6a6578f5f177b0df6dad0570fbed12f8ca
keane dasilva <keane dasilva>
2016-08-07 20:59:17 -0500

    added functionality to finish button in add activity

--- app/src/main/java/com/example/android/inventory/AddActivity.java
+++ app/src/main/java/com/example/android/inventory/AddActivity.java
@@ -1,10 +1,12 @@
 package com.example.android.inventory;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 
 
 public class AddActivity extends AppCompatActivity {
@@ -15,33 +17,43 @@ public class AddActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
 
-        int quantity = 0;
-        float price = 1;
-        boolean dataValid = false;
 
-        EditText enterName = (EditText) findViewById(R.id.enterName);
-        String prodcName = enterName.getText().toString();
-        EditText enterQuantity = (EditText) findViewById(R.id.enterQuantity);
-        EditText enterPrice = (EditText) findViewById(R.id.enterPrice);
+        final EditText enterName = (EditText) findViewById(R.id.enterName);
+        final EditText enterQuantity = (EditText) findViewById(R.id.enterQuantity);
+        final EditText enterPrice = (EditText) findViewById(R.id.enterPrice);
 
-        if (prodcName.isEmpty()){
-            Toast invalidData = Toast.makeText(this,"No Product Name Given",Toast.LENGTH_SHORT);
-            invalidData.show();
-        }else{
-            try{
-                quantity = Integer.parseInt(enterQuantity.getText().toString());
-                price = Float.parseFloat(enterPrice.getText().toString());
-            }catch (Exception e){
-                Toast invalidData = Toast.makeText(this,"Invalid Data Entry",Toast.LENGTH_SHORT);
-                invalidData.show();
-                dataValid = true;
+        Button finishbutton = (Button) findViewById(R.id.finishbutton);
+        finishbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int quantity = 0;
+                float price = 1;
+                boolean dataValid = true;
+
+                String prodcName = enterName.getText().toString();
+
+                if (prodcName.isEmpty()){
+                    Toast invalidData = Toast.makeText(getApplicationContext(),"No Product Name Given",Toast.LENGTH_SHORT);
+                    invalidData.show();
+                    dataValid = false;
+                }else{
+                    try{
+                        quantity = Integer.parseInt(enterQuantity.getText().toString());
+                        price = Float.parseFloat(enterPrice.getText().toString());
+                    }catch (Exception e){
+                        Toast invalidData = Toast.makeText(getApplicationContext(),"Invalid Data Entry",Toast.LENGTH_SHORT);
+                        invalidData.show();
+                        dataValid =false;
+                    }
+                }
+
+                if (dataValid){
+                    DbUtils.insertNewProduct(prodcName,quantity,price,getApplicationContext());
+                    Intent returnIntent = new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(returnIntent);
+                }
             }
-        }
-
-        if (dataValid){
-            DbUtils.insertNewProduct(prodcName,quantity,price,this);
-        }
-
+        });
 
     }
 }
